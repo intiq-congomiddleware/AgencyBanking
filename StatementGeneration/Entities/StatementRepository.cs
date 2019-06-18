2b6b9fb99e5c684ba9293e1fd5b35aff48e2b568
donaldokafor <30751246+donaldokafor@users.noreply.github.com>
2019-06-18 17:39:45 +0100

    Statement repo Update
    Statement repo Update

--- StatementGeneration/Entities/StatementRepository.cs
+++ StatementGeneration/Entities/StatementRepository.cs
@@ -29,6 +29,8 @@ namespace StatementGeneration.Entities
         public async Task<Tuple<List<StatementResponse>, Response>> GenerateStatement(StatementRequest request)
         {
             List<StatementResponse> res = new List<StatementResponse>();
+            List<StatementResponseDTO> resDTO = new List<StatementResponseDTO>();
+
             Response _res = new Response();
             string reqString; string respMsg = string.Empty; string resultContent = string.Empty;
             int respCode = 0;
@@ -71,7 +73,22 @@ namespace StatementGeneration.Entities
             if (!string.IsNullOrEmpty(resultContent))
             {
                 _res = JsonHelper.fromJson<Response>(resultContent);
-                res = JsonHelper.fromJson<List<StatementResponse>>(resultContent);
+                resDTO = JsonHelper.fromJson<List<StatementResponseDTO>>(resultContent);
+                if (resDTO != null && resDTO.Count > 0)
+                {
+                    foreach (var r in resDTO)
+                    {
+                        res.Add(new StatementResponse()
+                        {
+                            balance = string.IsNullOrEmpty(r.RUNNING_BAL) ? 0 : Convert.ToDecimal(r.RUNNING_BAL),
+                            credit = string.IsNullOrEmpty(r.CR_AMT) ? 0 : Convert.ToDecimal(r.CR_AMT),
+                            debit = string.IsNullOrEmpty(r.DR_AMT) ? 0 : Convert.ToDecimal(r.DR_AMT),
+                            transactionDate = string.IsNullOrEmpty(r.TXN_DAT) ? Convert.ToDateTime("0001-01-01 00:00:00") : Convert.ToDateTime(r.TXN_DAT),
+                            transactionDesc = r.TXN_DESC,
+                            valueDate = string.IsNullOrEmpty(r.VAL_DT) ? Convert.ToDateTime("0001-01-01 00:00:00") : Convert.ToDateTime(r.VAL_DT)
+                        });
+                    }
+                }
             }
             else
             {
@@ -81,7 +98,7 @@ namespace StatementGeneration.Entities
 
             return new Tuple<List<StatementResponse>, Response>(res, _res);
         }
-     
+
 
         public string EncData(string value)
         {
---
+++ StatementGeneration/Entities/StatementResponseDTO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatementGeneration.Entities
+{
+    public class StatementResponseDTO
+    {
+        public string RUN_USERID { get; set; }
+        public string ACCT_NO { get; set; }
+        public string TXN_DAT { get; set; }
+        public string VAL_DT { get; set; }
+        public string TXN_BRN { get; set; }
+        public string COD_USER_ID { get; set; }
+        public string TXN_DESC { get; set; }
+        public string REF_NO { get; set; }
+        public string DR_AMT { get; set; }
+        public string CR_AMT { get; set; }
+        public string RUNNING_BAL { get; set; }
+        public string DAT_POST { get; set; }
+        public string TRANSACTION_REFERENCE { get; set; }
+        public string TRAN_MNEMONIC { get; set; }
+        public string TRANS_SEQNO { get; set; }
+        public string RUNDATE { get; set; }
+        public string STARTDATE { get; set; }
+        public string ENDDATE { get; set; }
+        public string TOTALDR { get; set; }
+        public string TOTALCR { get; set; }
+        public string DRCOUNT { get; set; }
+        public string CRCOUNT { get; set; }
+        public string CURRENCY { get; set; }
+        public string CUSTOMERNAME { get; set; }
+        public string OPENINGBALANCE { get; set; }
+        public string CLOSINGBALANCE { get; set; }
+        public string ADDRESS { get; set; }
+        public string BRANCH_CODE { get; set; }
+        public string ACCOUNT_CLASS { get; set; }
+        public string AC_STAT_NO_DR { get; set; }
+        public string AC_STAT_NO_CR { get; set; }
+        public string AC_STAT_BLOCK { get; set; }
+        public string AC_STAT_STOP_PAY { get; set; }
+        public string AC_STAT_DORMANT { get; set; }
+        public string AC_STAT_FROZEN { get; set; }
+        public string AC_STAT_DE_POST { get; set; }
+        public string ACY_AVL_BAL { get; set; }
+    }
+}
